393b41ecba8b54702be63f9b125862ad37a9c90a
thedurlav <thedurlav@gmail.com>
2024-04-27 21:51:28 +0530

    commits two

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <Form />
+      <PackagingList />
+      <Stats />
     </div>
   );
-}
+};
 
 export default App;
+
+const Logo = () => {
+  return <h1> 🌴 Far Away 🥥</h1>;
+};
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 🎃 trip?</h3>
+    </div>
+  );
+};
+
+const PackagingList = () => {
+  return <div className="List">LIST</div>;
+};
+
+const Stats = () => {
+  return (
+    <footer>
+      <em>
+        {" "}
+        🪭 You have X items on your list, and you are already packed X (X%)
+      </em>
+    </footer>
+  );
+};
---
+++ src/Logo.jsx
@@ -0,0 +1,5 @@
+const Logo = () => {
+  return <h1> 🌴 Far Away 🥥</h1>;
+};
+
+export default Logo;
